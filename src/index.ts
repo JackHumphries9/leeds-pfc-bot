db0881aea3602e886b9c6d239a9b0887ce3caade
Jack Humphries <jackphumphries02@gmail.com>
2023-02-01 11:11:04 +0000

    Added a basic bot template

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-const main = () => {
-	console.log("Hello World!");
-};
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
-main();
+const TOKEN = process.env.TOKEN;
+
+if (!TOKEN) {
+	throw new Error("No token provided");
+	process.exit(1);
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(TOKEN);
