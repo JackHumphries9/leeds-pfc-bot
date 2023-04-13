7e53f05dd30673f79053d8322226e1b5c40e7ddb
Jack Humphries <jackphumphries02@gmail.com>
2023-04-13 16:47:53 +0100

    Added potential fix for guilds that are not cached

--- src/utils/logAction.ts
+++ src/utils/logAction.ts
@@ -1,9 +1,22 @@
 import { Client, TextChannel } from "discord.js";
-import { logError } from "./logger";
+import { info, logError } from "./logger";
 import config from "../config";
 
-export const logAction = (action: string, client: Client) => {
+export const logAction = (action: string, client: Client, tr?: boolean) => {
 	const guild = client.guilds.cache.get(config.guildId);
+
+	if (!guild && !tr) {
+		client.guilds.fetch().then(() => {
+			info("Attempting to log action again and refresh guilds cache");
+			logAction(action, client, true);
+		});
+		return;
+	}
+
+	if (tr) {
+		logError("Guild not found");
+		return;
+	}
 
 	const ch = guild.channels.cache.find(
 		(c) => c.id === config.logChannelId
