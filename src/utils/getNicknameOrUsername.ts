8b455f47128a9c73adf686f48b9a1ff888a76e39
Jack Humphries <jackphumphries02@gmail.com>
2024-01-12 16:24:01 +0000

    Fixed bug with nickname

--- src/utils/getNicknameOrUsername.ts
+++ src/utils/getNicknameOrUsername.ts
@@ -1,5 +1,7 @@
 import { GuildMember } from "discord.js";
 
-export const getNicknameOrUsername = (member: GuildMember) => {
+export const getNicknameOrUsername = (member?: GuildMember) => {
+	if (!member) return "Unknown";
+
 	return member.nickname || member.user.username;
 };
